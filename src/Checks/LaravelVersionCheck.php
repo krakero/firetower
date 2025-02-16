56386613c22cc8551ba92d0d662ef51ba458a7ef
Vince Mitchell <vincent.mitchell@gmail.com>
2025-02-16 15:10:37 -0500

    improve check

--- src/Checks/LaravelVersionCheck.php
+++ src/Checks/LaravelVersionCheck.php
@@ -8,14 +8,34 @@ class LaravelVersionCheck extends Check
 
     public string $description = 'Gathers Laravel version for server side evaluation';
 
+    public bool $only_major = false;
+
+    public bool $only_minor = false;
+
     public function handle(): string
     {
         $this->pass();
 
         $this->data([
             'laravel_version' => $ver = app()->version(),
+            'only_major' => $this->only_major,
+            'only_minor' => $this->only_minor,
         ]);
 
         return $ver;
+    }
+
+    public function onlyMajor()
+    {
+        $this->only_major = true;
+
+        return $this;
+    }
+
+    public function onlyMinor()
+    {
+        $this->only_minor = true;
+
+        return $this;
     }
 }
